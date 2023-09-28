8cf2cb0da953e7eb08a475b2cd8dc4a83519a2b7
Tyuiu.LomskinVI@outlook.com <winr2005@gmail.com>
2023-09-28 11:04:33 +0500

    Task6

---
+++ Tyuiu.LomakinVI.Sprint1.Task6.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LomakinVI.Sprint1.Task6.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LomakinVI.Sprint1.Task6.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb833fba-9681-47fd-a595-5a4487cdf6ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.LomakinVI.Sprint1.Task6.V2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LomakinVI.Sprint1.Task6.V2.Lib;
+
+namespace Tyuiu.LomakinVI.Sprint1.Task6.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Ломакин В. И. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Ломакин Владислав Игоревич | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, есть ли в     *");
+            Console.WriteLine("* строке слово Hello                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (ds.CheckHello(str))
+            {
+                Console.WriteLine("Слово Hello есть в строке");
+
+            }
+            else
+            {
+                Console.WriteLine("Слова Hello нет в строке");
+            }
+            Console.ReadKey();  
+        }
+    }
+}
